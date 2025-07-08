c2ef05858db5e021570c6324178b3612a606961e
Tusharbecoding <tusharchopra02122002@gmail.com>
2025-07-08 11:32:56 +0530

    setting up parser

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from typing import List
 
 load_dotenv()
 
-# llm = ChatOpenAI(model="gpt-4o-mini")
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: List[str]
+    tools_used: List[str]
+
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
-response = llm.invoke("What is the capital of France?")
-print(response)
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
